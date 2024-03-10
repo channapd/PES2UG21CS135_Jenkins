24dbdda7d4b683115c2d1485196a03bdf093ecc8
channapd <55906710+channapd@users.noreply.github.com>
2024-03-10 13:13:27 +0530

    Update hello.cpp

--- main/hello.cpp
+++ main/hello.cpp
@@ -5,7 +5,6 @@ using namespace std;
 int main(void) {
   cout << "Hello, World\n";
   cout << "Hello, Jenkins\n";
-  cout << "Hello, PES2UG21CS135\n";
   cout << "I have successfully built and run\n";
   return 0;
 }
